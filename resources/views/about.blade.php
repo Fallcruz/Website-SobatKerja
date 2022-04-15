a9b98186e5186eef37b546f66348b7a824be4f64
Rizky Khoiruddin <rizky.khoiruddin17@gmail.com>
2022-04-15 12:39:57 +0700

    Menambah view index dan about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>SobatKerja</title>
+    <link rel="stylesheet" href="css/about.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <!--Navbar Atas-->
+    <div class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid px-4">
+        <a class="navbar-brand mx-4" href="/"
+          ><span class="sobat">Sobat</span><span class="kerja">Kerja</span></a
+        >
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item me-4">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="nav-item me-4">
+            <a class="nav-link active" href="/about">About</a>
+          </li>
+          <li class="nav-item me-4">
+            <a class="nav-link" href="">Contact</a>
+          </li>
+          <li class="nav-item me-3">
+            <button class="btn-pencari" type="button">
+              <a href="login_pencari.html">Pencari</a>
+            </button>
+          </li>
+          <li class="nav-item me-2">
+            <button class="btn-penyedia" type="button">
+              <a href="login_penyedia.html">Penyedia</a>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="container col-lg-8 mx-auto headline-about">
+      <div class="d-flex">
+        <div>
+          <div class="garis-biru"></div>
+          <h3 class="judul-about">ABOUT</h3>
+          <p class="desc-about">
+            Tujuan dibangunnya website ini adalah untuk memenuhi Tugas Besar
+            dari Mata Kuliah RPL Desain dan Implementasi Telkom University.
+            Website ini merupakan tempat dimana perusahaan-perusahaan dapat
+            menaruh atau membuka lowongan pekerjaan dan pencari kerja dapat
+            melihat atau bahkan melamar pekerjaan yang ia sukai.
+          </p>
+        </div>
+        <img src="Gambar/logo-fif.png" width="260px" />
+      </div>
+    </div>
+  </body>
+</html>
